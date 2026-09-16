a8372062e3be99ac795ff9e4f3e2b0dad99343d5
twoheartsservant-youngservants <twoheartsservant@gmail.com>
2026-09-16 18:47:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.YSL_API_URL='https://script.google.com/macros/s/AKfycbyoxnlxtjh5_L3Vmkg9BODwzdBf8-V7_M4MswdvyPwgFi2R9ECF-Wg-N2PpKdvLMOLk/exec';
+window.YSL_API_URL='https://script.google.com/macros/s/AKfycbwjVz3oZs732zR3kCIXAOX-bdLHdVVOXZEQkzVhA35ZdGYPwKtAwvQPLx4FFISNgFqU/exec';
